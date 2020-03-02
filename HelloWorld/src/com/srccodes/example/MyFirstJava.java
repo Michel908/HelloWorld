0ee77afda6e5158d3c15aaa31dd0c216b39aff08
michael.vetterolf <michael.vetterolf@qgrp.com>
2020-03-02 15:49:14 +0100

    Zweites Programm hinzugefügt

---
+++ HelloWorld/src/com/srccodes/example/MyFirstJava.java
@@ -0,0 +1,12 @@
+package com.srccodes.example;
+
+public class MyFirstJava {
+
+	/**
+	 * @param args heute
+	 */
+	public static void main(String[] args) {
+		System.out.println("My First Java Program");
+	}
+
+}
